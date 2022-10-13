f319951106df209e65bc497449b0438b822eb990
luigicr95 <112467906+luigicr95@users.noreply.github.com>
2022-10-13 18:59:36 +0200

    final update

--- client/client.js
+++ client/client.js
@@ -1,13 +1,30 @@
-async function displayHelloWorld() {
-  let helloHttpResponse = await fetch("http://localhost:3000/hello");
+async function displayDiscography() {
+  let discographyResponse = await fetch("http://localhost:3000/hello");
 
-  let text = await helloHttpResponse.text();
+  let discographyArray = await discographyResponse.json();
 
-  document.querySelector("body").innerHTML = text;
+  document.querySelector(".row").innerHTML = "";
 
-  console.log(text);
+  for (let album of discographyArray) {
+    let searchQuery = document.getElementById("search-bar").value;
+
+    if (
+      album.titolo.toLowerCase().includes(searchQuery) ||
+      album.autore.toLowerCase().includes(searchQuery)
+    ) {
+      document.querySelector(".row").innerHTML += `<div class="card  col-3">
+      <img src="${album.cover}" class="card-img-top pt-2" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${album.titolo}</h5>
+        <p class="card-text">
+          ${album.autore}
+        </p>
+      </div>
+    </div>`;
+    }
+  }
 }
 
 window.onload = () => {
-  displayHelloWorld();
+  displayDiscography();
 };
--- server/server.js
+++ server/server.js
@@ -6,7 +6,56 @@ const port = 3000;
 app.use(cors());
 
 app.get("/hello", (req, res) => {
-  res.send("Hello World!");
+  res.send([
+    {
+      id: 1,
+      cover: "../images/Bends.jpg",
+      titolo: "The Bends",
+      autore: "Radiohead",
+      dataDiUscita: "1995",
+      casaDiscografica: "EMI",
+    },
+    {
+      id: 2,
+      cover: "../images/Banquet.jpg",
+      titolo: "Beggars Banquet",
+      autore: "The Rolling Stones",
+      dataDiUscita: "1968",
+      casaDiscografica: "Decca Records",
+    },
+    {
+      id: 3,
+      cover: "../images/Radici.jpg",
+      titolo: "Radici",
+      autore: "Francesco Guccini",
+      dataDiUscita: "1972",
+      casaDiscografica: "Columbia Graphophone Company",
+    },
+    {
+      id: 4,
+      cover: "../images/Darkside.jpg",
+      titolo: "The Dark Side of the Moon",
+      autore: "Pink Floyd",
+      dataDiUscita: "1973",
+      casaDiscografica: "Capitol Records",
+    },
+    {
+      id: 5,
+      cover: "../images/Zero.jpg",
+      titolo: "0",
+      autore: "Low Roar",
+      dataDiUscita: "2014",
+      casaDiscografica: "Tonequake Records",
+    },
+    {
+      id: 6,
+      cover: "../images/Loretape.jpg",
+      titolo: "LORETAPE",
+      autore: "Morgan no Rarity",
+      dataDiUscita: "2021",
+      casaDiscografica: "Indipendente",
+    },
+  ]);
 });
 
 app.listen(port, () => {
